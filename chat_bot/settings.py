947b15fb085afb21be88905ce6d99777294ce4bc
rithin45 <127735094+rithin45@users.noreply.github.com>
2025-11-27 09:06:55 +0530

    mysql setup complete

--- chat_bot/settings.py
+++ chat_bot/settings.py
@@ -92,11 +92,11 @@ WSGI_APPLICATION = 'chat_bot.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': os.environ.get("railway"),
-        'USER': os.environ.get("root"),
-        'PASSWORD': os.environ.get("mogceOmKCYKNCYluioeeQSOfskFwzQQy"),
-        'HOST': os.environ.get("metro.proxy.rlwy.net"),
-        'PORT': os.environ.get("52614"),
+        'NAME': os.environ.get("DB_NAME"),
+        'USER': os.environ.get("DB_USER"),
+        'PASSWORD': os.environ.get("DB_PASSWORD"),
+        'HOST': os.environ.get("DB_HOST"),
+        'PORT': os.environ.get("DB_PORT"),
     }
 }
 
